0d00015a9e2d57a0de08de52c34345f5e6c4cde4
Adrian Perez <adrian1804perez@gmail.com>
2022-08-03 18:23:10 -0700

    Add getComputerChoice(fully complete)

--- javascript.js
+++ javascript.js
@@ -1,12 +1,19 @@
+computerChoice = undefined;
+
 function getComputerChoice () {
      //We need the computer to get a number from 0 - 2//
     let max = 2;
     let min = 0;
     let randNum = Math.floor(Math.random()*(max-min+1));
-    //If the number is 0 return rock//
-
-    //If the number is 1 return scissors//
-    //If the number is 2 return paper//
-
+    //If the number is 0 the choice is rock//
+    if (randNum === 0) {
+        computerChoice = "rock";
+    } else if (randNum === 1) {
+     //If the number is 1 then the choice is paper//
+        computerChoice = "paper";
+    } else {
+        //If the number is 2 then the choice is scissors//
+        computerChoice = "scissors";
+    }   
+    console.log (computerChoice);
 }
-
