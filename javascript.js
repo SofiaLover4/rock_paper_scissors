9473b9f06db4df91a2060eb438654366c2b71a22
Adrian Perez <adrian1804perez@gmail.com>
2022-08-03 17:31:32 -0700

    Add getComputer choice function and random number generation

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function getComputerChoice () {
+    //We need the computer to get a number from 0 - 2//
+    let randNum = Math.floor(Math.random()*100);
+    //If the number is 0 return rock//
+    //If the number is 1 return scissors//
+    //If the number is 2 return paper//
+}
