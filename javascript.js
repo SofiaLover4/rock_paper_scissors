4c8ffcd60d859f88bc5bb73226ddf731036012be
Adrian Perez <adrian1804perez@gmail.com>
2022-08-03 17:36:39 -0700

    Adjust random number generator

--- javascript.js
+++ javascript.js
@@ -1,7 +1,9 @@
 function getComputerChoice () {
     //We need the computer to get a number from 0 - 2//
-    let randNum = Math.floor(Math.random()*100);
+    let randNum = Math.floor(Math.random()*10) - 1;
     //If the number is 0 return rock//
     //If the number is 1 return scissors//
     //If the number is 2 return paper//
 }
+
+
