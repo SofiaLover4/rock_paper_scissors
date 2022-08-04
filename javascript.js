172b34b9cecc9479e66bd9eb204c7600ad1c01f8
Adrian Perez <adrian1804perez@gmail.com>
2022-08-04 10:05:58 -0700

    Add game function and final message
    OK so I got confused and completely forgot to commit anything. I was
    heavily struggling with the looping aspect and how to add scores but by
    the end of it I got the gist of it. I think. The code could probably be
    cleaner in some aspects but I'm happy it worked out in the end.

--- javascript.js
+++ javascript.js
@@ -26,16 +26,56 @@ function playRound (playerSelection,computerSelection) {
     let tieMessage = () => console.log("It's a tie, I guess none of us win");
     //Capitalization cannot effect the outcome//
     playerSelection = playerSelection.toLowerCase();
-    //Messages for each outcome of the game//
-    if (playerSelection === computerSelection) {
-        tieMessage();  
-    }else if (playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock" ||playerSelection === "rock" && computerSelection === "scissors") {
+    //A message in case the word is not any of the possible words//
+    let errorMessage = () => console.log("Hey! That's not a word you can use in this game!")
+    //Return the user a message if they word they put is not in the game//
+    if (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {  
+        errorMessage();
+        roundStatus = null;
+    //All the outcomes for the game//
+    } else if (playerSelection === computerSelection) {
+        tieMessage(); 
+        roundStatus = null;
+    } else if (playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock" ||playerSelection === "rock" && computerSelection === "scissors") {
         winMessage();
+        roundStatus = true;
     } else if (playerSelection === "paper" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "rock" || playerSelection === "rock" && computerSelection === "paper") {
         loseMessage();
+        roundStatus = false;
     }
+   
 }
 
-let playerSelection = "scissors";
-let computerSelection = getComputerChoice();
-playRound(playerSelection, computerSelection);
+let finalMessage = (finalWinner,finalLoser,winnerScore,loserScore) => console.log(`And the winner is ${finalWinner} with ${winnerScore} games won. The loser is ${finalLoser} with ${loserScore} games won.`)
+
+
+function game () {
+    let rounds = 0;
+    let playerCount = 0;
+    let computerCount = 0;
+    for (let i = 0; rounds < 5; i++){
+        computerSelection = getComputerChoice();
+        playerSelection = prompt("Rock, paper, scissors, shoot!")
+        playRound(playerSelection,computerSelection);
+        if (roundStatus === true){
+            playerCount++;
+            rounds++;
+        } else if (roundStatus === false) {
+            computerCount++; 
+            rounds++;
+        }else if (roundStatus === null){
+            console.log("Hmmm, let's not count this round then.")
+        }
+        console.log(`Round: ${rounds}`);
+        console.log(`Your score: ${playerCount}`);
+        console.log(`Opponent's score: ${computerCount}`);
+    }
+    if (playerCount > computerCount){
+        finalMessage("you","me",playerCount,computerCount);
+    }else {
+        finalMessage("me","you", computerCount,playerCount);
+    }
+
+
+    }
+game();
