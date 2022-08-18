9f4cc27b2c1e72ce94e8102003f13b700f10d60a
Adrian Perez <adrian1804perez@gmail.com>
2022-08-18 08:26:03 -0700

    Add exit and reset buttons

--- javascript.js
+++ javascript.js
@@ -15,11 +15,16 @@ const rounds = document.querySelector('.Rounds');
 const enemyScore = document.querySelector('.Computer_Score');
 const matchWinner = document.querySelector('.winner');
 const startBtn = document.querySelector('.start_btn');
+const endContent = document.querySelector('.endContent');
+const exitBtn = document.querySelector('.exitBtn');
+const resetBtn = document.querySelector('.resetBtn');
 
 rockBtn.addEventListener('click',playRoundRock);
 paperBtn.addEventListener('click',playRoundPaper);
 scissorsBtn.addEventListener('click', playRoundScissors);
 startBtn.addEventListener('click',startGame);
+resetBtn.addEventListener('click',resetMatch);
+exitBtn.addEventListener('click',exitGame);
 
 function displayStats (){
     yourScore.textContent = `Your score is ${playerScore}`;
@@ -44,6 +49,79 @@ function getComputerChoice () {
         //If the number is 2 then the choice is scissors//
         return computerChoice = "scissors";
     }  
+}
+
+function playRoundRock () {
+    playRound("rock",getComputerChoice())
+}
+
+function playRoundPaper () {
+    playRound("paper",getComputerChoice())
+}
+
+function playRoundScissors () {
+    playRound("scissors",getComputerChoice())
+}
+
+function resetGame () {
+    playerScore = 0;
+    computerScore = 0;
+    gameRounds = 0;
+}
+
+function startGame () {
+    rockBtn.classList.toggle('game');
+    paperBtn.classList.toggle('game');
+    scissorsBtn.classList.toggle('game');
+    results.classList.toggle('game');
+    cards.classList.toggle('game');
+    startBtn.classList.toggle('game');
+    resetGame();
+}
+
+function checkWinner () {
+    if(playerScore === 5) {
+        winnerMessage("You","I");
+        endGame();
+    } else if (computerScore === 5) {
+        winnerMessage("I", "you")
+        endGame();
+    }
+}
+
+function toggleEndBtns () {
+    exitBtn.classList.toggle('game');
+    resetBtn.classList.toggle('game');
+    endContent.classList.toggle('game');
+}
+
+function endGame (){
+    rockBtn.removeEventListener('click', playRoundRock);
+    paperBtn.removeEventListener('click', playRoundPaper);
+    scissorsBtn.removeEventListener('click', playRoundScissors);
+    toggleEndBtns();
+
+}
+
+function resetMatch () {
+    toggleEndBtns();
+    resetGame();
+    displayStats();
+    rockBtn.addEventListener('click',playRoundRock);
+    paperBtn.addEventListener('click',playRoundPaper);
+    scissorsBtn.addEventListener('click', playRoundScissors);
+    result.textContent = "";
+    matchWinner.textContent = "";
+}
+
+function exitGame () {
+    rockBtn.classList.toggle('game');
+    paperBtn.classList.toggle('game');
+    scissorsBtn.classList.toggle('game');
+    results.classList.toggle('game');
+    cards.classList.toggle('game');
+    startBtn.classList.toggle('game');
+    resetMatch();
 }
 
 function playRound (playerSelection,computerSelection) {
@@ -77,41 +155,4 @@ function playRound (playerSelection,computerSelection) {
         checkWinner();
     }
    
-}
-
-function playRoundRock () {
-    playRound("rock",getComputerChoice())
-}
-
-function playRoundPaper () {
-    playRound("paper",getComputerChoice())
-}
-
-function playRoundScissors () {
-    playRound("scissors",getComputerChoice())
-}
-
-function startGame () {
-    rockBtn.classList.toggle('game');
-    paperBtn.classList.toggle('game');
-    scissorsBtn.classList.toggle('game');
-    results.classList.toggle('game');
-    cards.classList.toggle('game');
-    startBtn.classList.toggle('game');
-}
-
-function checkWinner () {
-    if(playerScore === 5) {
-        winnerMessage("You","I");
-        endGame();
-    } else if (computerScore === 5) {
-        winnerMessage("I", "you")
-        endGame();
-    }
-}
-
-function endGame (){
-    rockBtn.removeEventListener('click', playRoundRock);
-    paperBtn.removeEventListener('click', playRoundPaper);
-    scissorsBtn.removeEventListener('click', playRoundScissors);
 }
